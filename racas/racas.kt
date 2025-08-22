11c688bf04146e6a1fa62ff598ca0928f335d1b1
Julia Soupinski Rodrigues <164052139+juliasoup@users.noreply.github.com>
2025-08-22 14:12:17 -0300

    racas.kt

--- racas/racas.kt
+++ racas/racas.kt
@@ -3,6 +3,8 @@ package org.example.racas
 open class Raca(val nome: String, val fisico: String = "", val habilidade: String = "") {
 
     open fun exibirInfos() {
-        println("Nome: $nome, Físico: $fisico, Habilidade: $habilidade")
+        println("Nome: $nome")
+        println("Físico: $fisico")
+        println("Habilidade: $habilidade")
     }
 }
