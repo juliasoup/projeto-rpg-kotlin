182faee0a61371190451d6e48cdd952256d15ac1
Julia Soupinski Rodrigues <164052139+juliasoup@users.noreply.github.com>
2025-08-22 13:01:10 -0300

    Delete racas.kt

--- racas.kt
+++
@@ -1,8 +0,0 @@
-package org.example.racas
-
-open class Raca(val nome: String, val fisico: String = "", val habilidade: String = "") {
-
-    open fun exibirInfos() {
-        println("Nome: $nome, Físico: $fisico, Habilidade: $habilidade")
-    }
-}
